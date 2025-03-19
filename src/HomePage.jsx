8f651c7c9b0d38810d6da75d73562c406748fd8e
mander5 <aaron_mander@hotmail.com>
2025-03-19 10:40:29 +0000

    content mapping

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -3,6 +3,8 @@ import heroImage from './assets/hero-image.jpg';
 import logo from './assets/LDNlogowhite.png';
 import logoBlack from './assets/LDNlogo.png';
 import { useState } from 'react';
+import { FaTiktok, FaInstagram, FaFacebook, FaLinkedin } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
 
 export default function HomePage() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -61,7 +63,7 @@ export default function HomePage() {
           <h2>Who We Are</h2>
           <p>
             We connect brands with passionate sports fans through dynamic
-            advertising solutions.
+            advertising and sponsorship solutions.
           </p>
         </div>
         <div className='about-logo'>
@@ -74,16 +76,16 @@ export default function HomePage() {
         <h2>Our Services</h2>
         <div className='services-grid'>
           <motion.div className='service-card' whileHover={{ scale: 1.05 }}>
-            <h3>Stadium Advertising</h3>
-            <p>Engage fans live in the action.</p>
+            <h3>Sponsorship and Activations</h3>
+            <p>Create meaningful brand connections.</p>
           </motion.div>
           <motion.div className='service-card' whileHover={{ scale: 1.05 }}>
             <h3>Social Media Campaigns</h3>
             <p>Leverage digital platforms for maximum reach.</p>
           </motion.div>
           <motion.div className='service-card' whileHover={{ scale: 1.05 }}>
-            <h3>Sponsorship Activations</h3>
-            <p>Create meaningful brand connections.</p>
+            <h3>Stadium Advertising</h3>
+            <p>Engage fans live in the action.</p>
           </motion.div>
         </div>
       </section>
@@ -99,7 +101,56 @@ export default function HomePage() {
       <footer className='footer'>
         <div className='footer-content'>
           <img src={logo} alt='LDN Sport Social Logo' className='footer-logo' />
-          <p>LDN Sport Social - Your partner in sports advertising</p>
+          <p>
+            LDN Sport Social - Your partner in sports advertising and
+            sponsorship
+          </p>
+          <div className='company-info'>
+            <p>LDN Sport Social Limited</p>
+            <p>Unit 1, Kennet House, 8 Enterprise Way</p>
+            <p>London SW18 1GF UK</p>
+            <p>Company Registration No: 14487021</p>
+            <p>VAT Reg No. 430084532</p>
+            <p>©2022 All rights reserved.</p>
+          </div>
+
+          <nav className='footer-nav'>
+            <a
+              href='https://twitter.com/ldnsportsocial'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FaXTwitter />
+            </a>
+            <a
+              href='https://www.facebook.com/profile.php?id=100088593576706'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FaFacebook />
+            </a>
+            <a
+              href='https://www.instagram.com/ldn.sport.social/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FaInstagram />
+            </a>
+            <a
+              href='https://www.tiktok.com/@ldnsportsocial'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FaTiktok />
+            </a>
+            <a
+              href='https://www.linkedin.com/company/ldn-sport-social/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FaLinkedin />
+            </a>
+          </nav>
           <nav className='footer-nav'>
             <a href='#home'>Home</a>
             <a href='#about'>About Us</a>
